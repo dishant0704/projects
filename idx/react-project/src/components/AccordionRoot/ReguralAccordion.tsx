a54be27b4fa51c5671d9e24d534e6f96bbca6a4f
Ketan Sawant <ketandutt@gmail.com>
2026-08-19 21:58:39 +0530

    Tab code added
    Hi
    Code added for Tab code added
    Thanks
    Ketan

--- idx/react-project/src/components/AccordionRoot/ReguralAccordion.tsx
+++ idx/react-project/src/components/AccordionRoot/ReguralAccordion.tsx
@@ -1,13 +1,67 @@
+import { useEffect, useState } from 'react';
 import SubPageTemplate from '../page-templates/SubPageTemplate'
 import Accordion from '../UiComponents/accordion/Accordion'
 import type { AccordionItemData } from '../UiComponents/accordion/type'
 import AccordionForm from './AccordionForm';
+import type { TabItem } from '../UiComponents/Tabs/types';
+import Tabs from '../UiComponents/Tabs';
+
+const componentRegistry: Record<string, React.ComponentType<any>> = {
+  accList: Accordion,
+  accForm: AccordionForm,
+};
+
 interface ReguralAccordionProps {
   data?: AccordionItemData[];
 }
 const ReguralAccordion = ({
   data = [],
 }: ReguralAccordionProps) => {
+    const [tabs, setTabs] = useState<TabItem[]>([]);
+
+    const tabsData = [
+    {
+        "id": "accList",
+        "label": "Regural Accordion",
+        "props": {
+            "data": []
+        }
+    },
+    {
+        "id": "accForm",
+        "label": "Dynamic Accordion",
+        "props": {
+            "data": []
+        }
+    }
+]
+
+ useEffect(() => {
+    // if (!currpage) {
+    //   return;
+    // }
+
+    // const tabs:TabItem = currpage.data as TabsProps["items"];
+    const tabItems: TabItem[] = tabsData.map((tab) => {
+      const { id, label, props, data } = tab;
+
+      const Component = componentRegistry[id];
+
+      return {
+        id,
+        label,
+
+        component: Component ?? (() => <div>Component "{id}" not found</div>),
+
+        props: {
+          ...(props ?? {}),
+          data: data ?? [],
+        },
+      };
+    });
+
+    setTabs(tabItems);
+  }, []);
    
     return (
         <section className=''>
@@ -16,6 +70,7 @@ const ReguralAccordion = ({
                     <Accordion items={data} defaultActiveTab={0} />
                 </SubPageTemplate.Left>
                 <SubPageTemplate.Right>
+                    {/* <Tabs items={tabs} defaultActiveTab="regAcc" /> */}
                     <AccordionForm />
                 </SubPageTemplate.Right>
             </SubPageTemplate>
