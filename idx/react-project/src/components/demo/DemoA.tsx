5259a79cb0f1e9b856cec1f0051fdef8a23d15f2
Ketan Sawant <ketandutt@gmail.com>
2026-09-15 21:25:46 +0530

    Dynamic Accordion
    Hi
    Code committed for Dynamic Accordion
    Thanks

--- idx/react-project/src/components/demo/DemoA.tsx
+++ idx/react-project/src/components/demo/DemoA.tsx
@@ -1,13 +1,25 @@
 import React from 'react'
+import img01 from '/images/carousel-images/img_01.jpg'
+import style from './demo.module.css'
 interface Props {
     value:[],
     setValue: ()=> void;
 }
-const DemoA:React.FC<Props> = (Props) => {
-    const {value} = Props
+const DemoA:React.FC<Props> = () => {
+  const{componentWrapper} = style    
   return (
-    <div>
-      {value}
+    <div className={`${componentWrapper} py-4`}>
+      <div className="grid grid-cols-3 gap-4 align-items-start dark:bg-zinc-800 bg-zinc-700 p-4 text-stone-100">
+        <div className="">
+          <div className="bg-white p-2">
+            <img src={img01} className="d-block w-100" alt="..." />
+          </div>
+        </div>
+        <div className="col-span-2 v-aligh-center">
+         <h1 className=''>First slide label</h1>
+            <p>Some representative placeholder content for the first slide. Some representative placeholder content for the first slide.vSome representative placeholder content for the first slide.</p>
+          </div>
+      </div>
     </div>
   )
 }
--- idx/react-project/src/components/demo/DemoB.tsx
+++ idx/react-project/src/components/demo/DemoB.tsx
@@ -1,13 +1,29 @@
 import React from 'react'
+import img04 from '/images/carousel-images/img_04.jpg'
+import style from './demo.module.css'
 interface Props {
     value:[],
     setValue: ()=> void;
 }
 const DemoB:React.FC<Props> = (Props) => {
-  const {value} = Props
+  const{componentWrapper} = style    
   return (
-    <div>
-      {value}
+    <div className={`${componentWrapper} py-4 grid grid-cols-1`}>
+      <div className="grid grid-cols-3 gap-4 align-items-start dark:bg-zinc-800 bg-zinc-700 p-4 text-stone-100">
+        <div className="">
+          <div className="bg-white p-2">
+            <img src={img04} className="d-block w-100" alt="..." />
+          </div>
+        </div>
+        <div className="col-span-2 v-aligh-center">
+         <h1 className=''>First slide label</h1>
+            <p>Some representative placeholder content for the first slide. Some representative placeholder content for the first slide.vSome representative placeholder content for the first slide.</p>
+          </div>
+      </div>
+      <div className="clickable bullets">
+        <span className="bullet bullet-active" role="button" aria-label="Go to slide 1" aria-current="true">1</span>
+        <span className="bullet"  role="button" aria-label="Go to slide 2">1</span>
+      </div>
     </div>
   )
 }
